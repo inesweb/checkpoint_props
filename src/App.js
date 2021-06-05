69a1bda3776fa3dd88a2c00f4272502a657b5f24
ines <ines@1D-INES.DC-CAPES.COM>
2021-06-05 10:56:30 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react'
-//import logo from './logo.svg';
 import photo3 from './photo3.jpg';
-import photo2 from './photo2.jpg';
-import photo1 from './photo1.jpg';
+//import photo2 from './photo2.jpg';
+//import photo1 from './photo1.jpg';
 import Profile from './components/Profile/Profile'
-//import './App.css';
+//import './App.css';handleName={handleName}
 
 function App() {
- 
+ // const handleName = fullName => alert(fullName);
+ const img = {marginTop: '80px',overflow: 'hidden' ,width: '32%' }
+ const container ={textAlign :'center' , height: '100vh', backgroundColor: 'beige'}
   return (
   
-    <div style ={{textAlign :'center' , height: '100vh', backgroundColor: 'beige'}}>
-     <Profile fullName ='ines zekri' bio ='let"s code '  profession ='software engineer'>
-     <img style = {{marginTop: '80px',overflow: 'hidden' ,width: '32%'}} src={photo3}   />
+    <div style ={container}>
+     <Profile fullName ='ines zekri' bio ='let"s code '  profession ='software engineer' >
+     <img style = {img} src={photo3}   />
      </Profile>
    
     </div>
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import PropTypes from "prop-types";
-function ReactHeader(props) {
-    return <h1>React {props.version} Documentation</h1>;
-   }
+
 const Profile = (props)=> {
     const handleName = props.fullName;
     alert(handleName);
      
    // console.log(handleName)
-   
+   const container = {display: 'inline-grid'}
+   const h2  ={color:'red' , display: 'inline-block', fontSize : '23px',paddingInline: '12px'}
+   const h3  ={color:'red' , display: 'inline-block', fontSize : '23px',paddingInline: '12px'}
  return(
 
-    <div style ={{display: 'inline-grid'}}>
+    <div style ={container}>
        <div > {props.children} </div>
-        <h2 style={{color:'red' , display: 'inline-block', fontSize : '23px',paddingInline: '12px'}} >Name:     </h2>{props.fullName}
-        <h3 style={{color:'red' , display: 'inline-block', fontSize : '23px',paddingInline: '12px'}}> Bio:</h3>{props.bio}
-        <h3 style={{color:'red' , display: 'inline-block', fontSize : '23px',paddingInline: '12px'}}> Profession</h3>{props.profession}
-       
+        <h2 style={h2} >Name:     </h2>{props.fullName}
+        <h3 style={h3}> Bio:</h3>{props.bio}
+        <h3 style={h3}> Profession</h3>{props.profession}
+      
     </div>
  )
 
